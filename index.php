93e0526a34f337cfda7fa6195091c998ff4ee6ff
Alex Knight <alex.j.knight@stu.mmu.ac.uk>
2019-01-10 09:46:30 +0000

    Beginning lab sheet

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
+// The HTML starts here
+echo " 
+ <table border='1'>
+    <tr>
+        <td>Rectangle 1  Your Name and registration number</td>
+        <td>Rectangle 2</td>
+    </tr>
+    <tr>
+        <td>Rectangle 3</td>
+        <td>Rectangle 4</td>
+    </tr>
+    <tr>
+        <td>Rectangle 5</td> 
+        <td>Rectangle 6</td>
+    </tr>
+    <tr>
+        <td>Rectangle 7</td>
+        <td>Rectangle 8</td>
+    </tr>
+    <tr>
+        <td>Rectangle 9</td>
+        <td>Rectangle 10</td>
+    </tr>
+</table> 
+";
 ?>
-Something is wrong with the XAMPP installation :-(
