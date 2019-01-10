ba2eac23063087d510e536ef9da0f68e9789634c
Alex Knight <alex.j.knight@stu.mmu.ac.uk>
2019-01-10 10:18:59 +0000

    Completed up to R 4

--- index.php
+++ index.php
@@ -1,17 +1,41 @@
-<?php
-// The HTML starts here
-echo " 
- <table border='1'>
+<table border='1'>
     <tr>
-        <td>Rectangle 1  Your Name and registration number</td>
-        <td>Rectangle 2</td>
+        <td>
+            <h2>Rectangle 1</h2> 
+            <p>Alex Knight</p>
+            <p>18016816</p>
+            <p>Date: <?php echo date("D:M:Y") ?></p>
+         </td>
+        <td>
+            <h2>Rectangle 2</h2>
+            <p><?php echo (3+4+5) ?></p>
+        </td>
     </tr>
     <tr>
-        <td>Rectangle 3</td>
-        <td>Rectangle 4</td>
+        <td>
+            <h2>Rectangle 3</h2>
+            <p> Random Number:
+                <?php
+                srand(3034);
+                    echo rand();
+                ?></p>
+        </td>
+        <td>
+            <h2>Rectangle 4</h2>
+            <p>
+                <?php
+                    for ($index = 30;$index !== 40;$index++)
+                    {
+                        if($index % 2 == 0)echo sprintf("%d,",$index);
+                    }
+                ?>
+            </p>
+        </td>
     </tr>
     <tr>
-        <td>Rectangle 5</td> 
+        <td>
+            <h2>Rectangle 5</h2>
+        </td>
         <td>Rectangle 6</td>
     </tr>
     <tr>
@@ -23,5 +47,3 @@ echo "
         <td>Rectangle 10</td>
     </tr>
 </table> 
-";
-?>
